dfef7a143f825bc0b9cf5645da363cc400230661
Rhythm Jain <rhythmjain.work@gmail.com>
2026-03-13 21:40:11 +0530

    backend initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserReg.Data;
+using UserReg.DTOs;
+using UserReg.Models;
+
+
+namespace UserReg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register( RegUser dto)
+        {
+            var user = new User
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                Password = dto.Password
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+        [HttpGet("users")]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.ToList();
+
+            return Ok(users);
+        }
+    }
+}
